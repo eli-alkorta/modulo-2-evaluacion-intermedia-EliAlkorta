8075a606121f560c5bb47d40d75dfa655ed0335d
elviac94 <elviac94@gmail.com>
2020-03-10 17:33:10 +0100

    add everything except last if

--- js/main.js
+++ js/main.js
@@ -1,2 +1,55 @@
 'use strict';
 
+const chosenNumber = document.querySelector('#chosen-number');
+const button = document.querySelector('.button');
+const tip = document.querySelector('#tip');
+const attempts = document.querySelector('#attempts');
+
+let numberOfAttempts = 0;
+
+
+function getRandomNumber(max) {
+
+    return Math.round(Math.random() * max)
+}
+const randomNumber = getRandomNumber(100);
+
+console.log(randomNumber);
+
+function compareNumbers() {
+
+    if  (chosenNumber.value > randomNumber) {
+
+        tip.value = 'Demasiado alto'
+
+    } else if (chosenNumber.value < randomNumber) {
+
+        tip.value = "Demasiado bajo"
+
+    } else if (chosenNumber.value = randomNumber) {
+
+        tip.value = "¡Has ganado campeona!"
+
+    } else if (chosenNumber.value > 100 && chosenNumber.value < 0) {
+
+        tip.value = "El número debe estar entre 1 y 100"
+    }
+}
+
+function addAttempts() {
+
+if ('click') {
+
+attempts.value = numberOfAttempts += 1;
+
+}
+
+} 
+
+button.addEventListener('click', compareNumbers);
+button.addEventListener('click', addAttempts);
+
+
+
+
+
