9484cca46b0f3747a74c8e3136e98e6435d51408
elviac94 <elviac94@gmail.com>
2020-03-23 18:57:08 +0100

    fix issues

--- js/main.js
+++ js/main.js
@@ -1,57 +1,40 @@
-'use strict';
+"use strict";
 
-const chosenNumber = document.querySelector('#chosen-number');
-const button = document.querySelector('.button');
-const tip = document.querySelector('#tip');
-const attempts = document.querySelector('#attempts');
+const chosenNumber = document.querySelector("#chosen-number");
+const button = document.querySelector(".button");
+const tip = document.querySelector("#tip");
+const attempts = document.querySelector("#attempts");
 
 let numberOfAttempts = 0;
 
-
 function getRandomNumber(max) {
-
-    return Math.round(Math.random() * max)
+  return Math.round(Math.random() * max);
 }
 const randomNumber = getRandomNumber(100);
 
 console.log(randomNumber);
 
+function buttonHandler() {
+  compareNumbers();
+  addAttempts();
+}
+
 function compareNumbers() {
-
-    if (chosenNumber.value > 100 || chosenNumber.value < 0) {
-
-        tip.value = "No está entre 1 y 100"
-
-    } else if  (chosenNumber.value > randomNumber) {
-
-        tip.value = 'Demasiado alto'
-
-    } else if (chosenNumber.value < randomNumber) {
-
-        tip.value = "Demasiado bajo"
-
-    } else if (chosenNumber.value = randomNumber) {
-
-        tip.value = "¡Has ganado campeona!"
-
-    }
-    
+  if (chosenNumber.value > 100 || chosenNumber.value < 0) {
+    tip.value = "No está entre 1 y 100";
+  } else if (chosenNumber.value > randomNumber) {
+    tip.value = "Demasiado alto";
+  } else if (chosenNumber.value < randomNumber) {
+    tip.value = "Demasiado bajo";
+  } else {
+    tip.value = "¡Has ganado campeona!";
+  }
 }
 
 function addAttempts() {
-
-if ('click') {
-
-attempts.value = numberOfAttempts += 1;
-
+  if ("click") {
+    attempts.value = numberOfAttempts += 1;
+  }
 }
 
-} 
-
-button.addEventListener('click', compareNumbers);
-button.addEventListener('click', addAttempts);
-
-
-
-
-
+button.addEventListener("click", buttonHandler);
